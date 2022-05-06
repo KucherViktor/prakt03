1b619854b2f82528c4707684ecddc2aba401f23b
Kucher Victor <kucher@class.ztk.org.ua>
2022-05-06 19:56:53 +0300

    Made Task2 + refinement Task1

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
 let canvas = document.querySelector('.canvas');
 let colorPalette = document.querySelector('.color_palette');
-let color=getComputedStyle(colorPalette.children[0]).backgroundColor;
+let color=window.getComputedStyle(colorPalette.children[0]).backgroundColor;
 let previousBlock=colorPalette.children[0];
 
 colorPalette.addEventListener('click',function(event){
-    previousBlock.classList.remove('active');
-    color=getComputedStyle(event.target).backgroundColor;
-    event.target.classList.add('active');
-    previousBlock=event.target;
+    if(event.target.classList.contains('color_block')){
+        previousBlock.classList.remove('active');
+        color=window.getComputedStyle(event.target).backgroundColor;
+        event.target.classList.add('active');
+        previousBlock=event.target;
+    }
 })
 
 canvas.addEventListener('click',function(event){
     if(event.target.classList.contains('pixel'))
         event.target.style.backgroundColor=color;  
 })
+let card = document.querySelectorAll('.card');
+for(let i=0;i<card.length;i++){
+    card[i].addEventListener('click',function(){
+        if(card[i].classList.contains('rotate_front')){
+            card[i].classList.remove('rotate_front')
+            card[i].classList.add('rotate_back')
+        }
+        else{
+            card[i].classList.remove('rotate_back')
+            card[i].classList.add('rotate_front')
+        }
+    })
+}
