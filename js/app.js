9e5c0cb3596a6fe60b1a5df94ae15b1c08c0c8a5
Kucher Victor <kucher@class.ztk.org.ua>
2022-05-09 15:36:12 +0300

    code upgrade

--- js/app.js
+++ js/app.js
@@ -4,28 +4,21 @@ let color=window.getComputedStyle(colorPalette.children[0]).backgroundColor;
 let previousBlock=colorPalette.children[0];
 
 colorPalette.addEventListener('click',function(event){
-    if(event.target.classList.contains('color_block')){
-        previousBlock.classList.remove('active');
-        color=window.getComputedStyle(event.target).backgroundColor;
-        event.target.classList.add('active');
-        previousBlock=event.target;
-    }
+    if(event.target.classList.contains('color_palette'))
+        return;
+    previousBlock.classList.remove('active');
+    color=window.getComputedStyle(event.target).backgroundColor;
+    event.target.classList.add('active');
+    previousBlock=event.target;
 })
 
 canvas.addEventListener('click',function(event){
-    if(event.target.classList.contains('pixel'))
+    if(!event.target.classList.contains('canvas'))
         event.target.style.backgroundColor=color;  
 })
-let card = document.querySelectorAll('.card');
-for(let i=0;i<card.length;i++){
-    card[i].addEventListener('click',function(){
-        if(card[i].classList.contains('rotate_front')){
-            card[i].classList.remove('rotate_front')
-            card[i].classList.add('rotate_back')
-        }
-        else{
-            card[i].classList.remove('rotate_back')
-            card[i].classList.add('rotate_front')
-        }
-    })
-}
+
+let deckOfCards = document.querySelector('.deck_of_cards');
+deckOfCards.addEventListener('click',function(event){
+    if(!event.target.classList.contains('deck_of_cards'))
+        event.target.classList.toggle('rotate_card');
+})
